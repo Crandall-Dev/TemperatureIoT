d515aa8977f6d38a5f4ca4d4a5e31ff477616166
Aaron S. Crandall <crandall@gonzaga.edu>
2022-06-26 23:18:42 -0700

    Initial ESP8266 code and InfluxDB scribe code

---
+++ InfluxScribe/IoTScribe.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python3
+#
+#   IoT system MQTT listener and InfluxDB logger
+#
+#   Currently, quite simple for initial data collection
+#
+#   @author Aaron S. Crandall <crandall@gonzaga.edu>
+#   @copyright 2022
+#
+
+import paho.mqtt.client as mqtt
+from influxdb import InfluxDBClient
+from time import sleep
+import json
+import logging
+
+global influx_client
+global message_count
+message_count = 0;
+
+
+def upload_point(measurement, tag_location, field_value):
+    point = {
+        "measurement": measurement,
+        "tags": {
+            "deviceLocation": tag_location,
+        },
+        "fields": {
+            "value": field_value
+        }
+    }
+    # print(point)
+    influx_client.write_points([point])
+
+
+# ** Create a InfluxDB data point from an gyro message **
+def create_gyro_point(msg):
+    point = {
+        "measurement": "gyroscope",
+        "tags": {
+            "deviceLocation": msg["location"],
+        },
+        "fields": {
+            "x": msg["value"]["x"],
+            "y": msg["value"]["y"],
+            "z": msg["value"]["z"]
+        }
+    }
+    # print(point)
+    return point
+
+
+# ** Create a InfluxDB data point from an accel message **
+def create_accel_point(msg):
+    point = {
+        "measurement": "accelerometer",
+        "tags": {
+            "deviceLocation": msg["location"],
+        },
+        "fields": {
+            "x": msg["value"]["x"],
+            "y": msg["value"]["y"],
+            "z": msg["value"]["z"]
+        }
+    }
+    return point
+
+# ** Create a InfluxDB data point from a status message **
+def create_status_point(msg):
+    point = {
+        "measurement": "status",
+        "tags": {
+            "deviceLocation": msg["location"],
+        },
+        "fields": {
+            "online": msg["value"]
+        }
+    }
+    return point
+
+# ** Create a InfluxDB data point from a recording message **
+def create_recording_point(msg):
+    point = {
+        "measurement": "recording",
+        "tags": {
+            "deviceLocation": msg["location"],
+        },
+        "fields": {
+            "status": msg["value"]
+        }
+    }
+    return point
+
+# ** Create a InfluxDB data point from a temperature message **
+def createTemperaturePoint(dat):
+    point = {
+        "measurement": "temperature",
+        "tags": {
+            "deviceLocation": dat["location"],
+        },
+        "fields": {
+            "value": dat["value"]
+        }
+    }
+    return point
+
+# ** Create a InfluxDB data point from a humidity message **
+def createHumidityPoint(dat):
+    point = {
+        "measurement": "humidity",
+        "tags": {
+            "deviceLocation": dat["location"],
+        },
+        "fields": {
+            "value": dat["value"]
+        }
+    }
+    return point
+
+
+
+# ** ********************************************************
+def on_message(client, userdata, message):
+    global message_count
+    msg_decoded = str(message.payload.decode("utf-8"))
+
+    print(msg_decoded)
+
+    fields = msg_decoded.split(":")
+    dat = {
+        "deviceID": fields[0],
+        "location": fields[1],
+        "measurement": fields[2],
+        "value": fields[3]
+    }
+
+    newPoint = None
+    if dat["measurement"] == "temperature":
+        newPoint = createTemperaturePoint(dat)
+    elif dat["measurement"] == "humidity":
+        newPoint = createHumidityPoint(dat)
+
+    if(newPoint):
+        influx_client.write_points([newPoint])
+
+def on_connect(client, userdata, flags, rc):
+    logging.info("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("IoT/rawevents")
+
+if __name__ == "__main__":
+    formatter = "[%(asctime)s] %(name)s {%(filename)s:%(lineno)d} %(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.INFO, format=formatter)
+    logging.info("Starting logger.")
+
+    influx_client = InfluxDBClient(host='localhost', port=8086, database='IoT')
+    logging.info("Connected to InfluxDB")
+
+    # client.write_points(points)
+
+    mqtt_client = mqtt.Client("pyLogger")
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+    mqtt_client.enable_logger()
+
+    #mqtt_client.connect("localhost")
+    mqtt_client.connect("broker.local")
+
+    try:
+        logging.info("Connected to MQTT - starting processing")
+        mqtt_client.loop_forever()
+    except KeyboardInterrupt:
+        print("")
+        logging.info("Received interrupt - quitting")
+    finally:
+        mqtt_client.loop_stop()
+        mqtt_client.disconnect()
+        influx_client.close()
+        logging.info("Disconnected from MQTT & InfluxDB")
+    logging.info("Logger terminated.")
---
+++ TemperatureIoT/TemperatureIoT.ino
@@ -0,0 +1,232 @@
+/**
+ * Temperature / Env IoT node
+ * 
+ * ESP8266 core
+ * SHT30 Env/Baro/Temp sensor
+ * 3D printed case
+ * MQTT client - broadcasting state
+ * 
+ * Author: Aaron S. Crandall <crandall@gonzaga.edu>
+ * Copyright: 2022
+ * 
+ */
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+#include "Adafruit_SHT31.h"
+
+// WiFi
+// Values set by 3rd party tool as to not check them into the git repo here
+//const char *ssid = "netssid";
+//const char *password = "passwd";
+
+// MQTT Broker
+const char *mqtt_broker = "10.0.0.4";
+const char *topic = "IoT/rawevents";
+//const char *mqtt_username = "emqx";
+//const char *mqtt_password = "public";
+const int mqtt_port = 1883;
+#define MQTT_HOSTNAME "broker.local"
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const int trigPin = 12;
+const int echoPin = 14;
+
+//define sound velocity in cm/uS
+#define SOUND_VELOCITY 0.034
+#define CM_TO_INCH 0.393701
+
+long duration;
+float distanceCm;
+float distanceInch;
+
+float samples[10];
+#define SAMPLES_COUNT 10
+#define INF_DISTANCE_CM 100
+
+enum DoorStatus {
+  IS_CLOSED,
+  IS_OPEN,
+};
+
+unsigned int last_report_epoch;
+#define REPORT_TIMEOUT_MS 60000
+
+char * g_location = "spaceport";
+char * g_sensorType = "garagedoor";
+char g_id[13] = "";
+
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+
+bool g_led_status = false;
+
+// ** ************************************************************
+// Onboard LED API / Controls
+void ledON() {
+  g_led_status = true;
+  digitalWrite(LED_BUILTIN, LOW); // 8266's are backwards
+}
+
+void ledOFF() {
+  g_led_status = false;
+  digitalWrite(LED_BUILTIN, HIGH); // 8266's are backwards
+}
+
+void ledToggle() {
+  if(g_led_status) {
+    ledOFF();
+  } else {
+    ledON();
+  }
+}
+
+
+// ** ************************************************************
+void setupMQTTConnection() {
+  //connecting to a mqtt broker
+  Serial.println("Connecting to MQTT Broker");
+  
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(mqtt_queue_callback);
+  while (!client.connected()) {
+    String client_id = "esp8266-client-";
+    client_id += String(WiFi.macAddress());
+    Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+    if (client.connect(client_id.c_str())) {
+        Serial.println("MQTT broker connected");
+    } else {
+        Serial.print("failed with state ");
+        Serial.print(client.state());
+        delay(2000);
+    }
+  }
+
+  // client.subscribe(topic);
+}
+
+
+// ** ************************************************************
+void setupDeviceID() {
+  byte mac[6];
+  WiFi.macAddress(mac);
+  sprintf(g_id, "%02X%02X%02X%02X%02X%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  Serial.printf("Device uniqe id: %s\n", g_id);
+  
+}
+
+
+// ** ************************************************************
+void mqtt_queue_callback(char *topic, byte *payload, unsigned int length) {
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+      Serial.print((char) payload[i]);
+  }
+  Serial.println();
+  Serial.println("-----------------------");
+}
+
+
+// ** ************************************************************
+void handleNetworkStatus() {
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.disconnect();
+    WiFi.begin();
+
+    Serial.print("Connecting to WiFi...");
+    while (WiFi.status() != WL_CONNECTED) {
+      ledToggle();
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("Connected to the WiFi network");
+
+    setupMQTTConnection();
+    ledOFF();
+  }
+}
+
+
+// ** ************************************************************
+void runReport() {
+  char message[50];
+  char value[10];
+
+  float temperature_C = sht31.readTemperature();
+  float humidity_pct = sht31.readHumidity();
+
+  char * temperatureTypeName = "temperature";
+  char * humidityTypeName = "humidity";
+  
+
+  sprintf(message, "%s:%s:%s:%0.2f", g_id, g_location, temperatureTypeName, temperature_C);
+  client.publish(topic, message);
+
+  Serial.println(message);
+
+  sprintf(message, "%s:%s:%s:%0.2f", g_id, g_location, humidityTypeName, humidity_pct);
+  client.publish(topic, message);
+  
+  Serial.println(message);
+
+}
+
+
+// ** ************************************************************
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  for( int i = 0; i < 4; i++ ) {
+    ledToggle();
+    delay(1000);
+  }
+
+  setupDeviceID();
+
+  handleNetworkStatus();
+
+  Serial.println("SHT31 Connect and configure");
+  if (! sht31.begin(0x45)) {   // Set to 0x45 for alternate i2c addr
+    Serial.println("Couldn't find SHT31");
+    while (1) delay(1);
+  }
+
+  last_report_epoch = millis();
+
+  Serial.println("Beginning normal operations after Wifi Connect");
+
+  delay(5000);
+  runReport();
+}
+
+
+// ** ************************************************************
+void loop() {
+  handleNetworkStatus();  // Check for disconnection & handle it
+
+
+  if (client.connected()==false){
+    Serial.println("MQTT Broker connection is down");
+    String client_id = "esp8266-client-";
+    client_id += String(WiFi.macAddress());
+    if (client.connect(client_id.c_str())) {
+       Serial.println("MQTT Broker Connection Restarted");
+    }
+  }
+  client.loop();    // Process/check MQTT queues
+
+  
+  if(last_report_epoch + REPORT_TIMEOUT_MS < millis()) {
+    runReport();
+    last_report_epoch = millis();
+  }
+
+}
